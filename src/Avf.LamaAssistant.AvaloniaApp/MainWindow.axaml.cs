ed9702d47c4f8ae6d386dc642ef8fc4860df8208
Markus Malz <markus.malz@outlook.com>
2025-07-12 08:25:14 +0200

    dotnet new avalonia.app -o .

---
+++ src/Avf.LamaAssistant.AvaloniaApp/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Avf.LamaAssistant.AvaloniaApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
