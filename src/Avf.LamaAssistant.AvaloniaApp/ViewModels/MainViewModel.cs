fcf9bf0e6e559e0f561fe3a72c37d55984bc35b4
Markus Malz <markus.malz@outlook.com>
2025-07-13 17:10:13 +0200

    prompt and data from view model

--- src/Avf.LamaAssistant.AvaloniaApp/ViewModels/MainViewModel.cs
+++ src/Avf.LamaAssistant.AvaloniaApp/ViewModels/MainViewModel.cs
@@ -9,23 +9,39 @@ public class MainViewModel : ViewModelBase
 {
    public ReactiveCommand<Unit, Unit> SayHelloCommand { get; }
 
-   private string _description = "string.Empty";
+    private string _prompt = "Answer with one word only. Give me the surname of a random German chancelor.";
+
+public string Prompt
+   {
+    get => _prompt;
+      set => this.RaiseAndSetIfChanged(ref _prompt, value);
+}
+
+    private string _description = "string.Empty";
    public string Description
    {
       get => _description;
       set => this.RaiseAndSetIfChanged(ref _description, value);
    }
+   private string _data = "";
+   public string Data
+   {
+      get => _data;
+      set => this.RaiseAndSetIfChanged(ref _data, value);
+   }
 
    public MainViewModel()
    {
+      //Prompt = "Answer with one word only. What is the german word for the english word rain?";
+      //Prompt = "Answer with one word only.";
+
       SayHelloCommand = ReactiveCommand.CreateFromTask(async () =>
         {
+           var ollamaToolkitWrapper = new OllamaToolkit.OllamaToolkitWrapper<string>();
            Description = "Thinking...";
-           await Task.Delay(1500); // Simulate async work
-           Description = $"Hello at {DateTime.Now:T}";
-
-           Avf.OllamaToolkit.OllamaToolkitWrapper<string> ollamaToolkitWrapper = new OllamaToolkit.OllamaToolkitWrapper<string>();
-           var result = await ollamaToolkitWrapper.GetOllamaAnswer("Answer with one word only. Give me the surname of a random German chancelor.");
+           //await Task.Delay(1500); // Simulate async work
+           //Description = $"Hello at {DateTime.Now:T}";           
+           var result = await ollamaToolkitWrapper.GetOllamaAnswer(Prompt, Data);
            Description = $"{result.Duration.TotalSeconds} {result.Value}";
         });
    }
