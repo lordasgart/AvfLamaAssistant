770effa135646c24ddff2697e9edaac14073e655
Markus Malz <markus.malz@outlook.com>
2025-07-13 09:47:07 +0200

    add messagebox

--- src/Avf.LamaAssistant.AvaloniaApp/MainWindow.axaml.cs
+++ src/Avf.LamaAssistant.AvaloniaApp/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 
 namespace Avf.LamaAssistant.AvaloniaApp;
 
@@ -13,5 +15,10 @@ public partial class MainWindow : Window
     private void SendButton_Click(object sender, RoutedEventArgs e)
     {
         // Add your button click event logic here
+        var box = MessageBoxManager
+            .GetMessageBoxStandard("Caption", "Are you sure you would like to delete appender_replace_page_1?",
+                ButtonEnum.YesNo);
+
+        var result = box.ShowAsync(); //Todo async!!!
     }
 }
